6e54f4cabe2d064bd79431470c25ea0ef8083aa3
jlplautz <jorge.plautz@gmail.com>
2025-08-20 16:42:04 -0300

    build(issue#4): installed additional libs

--- pydaria/app.py
+++ pydaria/app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 
 
 app = Flask(__name__)
 
+app.config['TITLE'] = 'Pydaria'
+Bootstrap(app)
+
 @app.route('/')
 def index():
     products = ["Pão Frances", "Bagguete", "Pretzel"]
-    return products[1]
+    return render_template('index.html', products=products)
